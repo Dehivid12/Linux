c49a73d025a417bcf3d3fa617588d3a514c1ef81
Dehivid12 <dehividsalgado2022@gmail.com>
2025-12-03 15:58:43 +0000

    Explicación de script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,11 @@
-#Crear lista de archivos
-echo "Hola Linux" > archivo.txt
-cat atchivo.txt
-cp archivo.txt copia.txt
-ls -l
-rm archivo.txt
+# Crear lista de archivos
+echo "Hola Linux" > archivo.txt     # Crea el archivo "archivo.txt" y escribe "Hola Linux" 
+
+cat archivo.txt                      # Muestra el contenido de "archivo.txt" en pantalla
+
+cp archivo.txt copia.txt             # Copia "archivo.txt" y crea un nuevo archivo llamado "copia.txt"
+
+ls -l                                # Hace una lista de los archivos del directorio en formato detallado
+
+rm archivo.txt                       # Elimina el archivo "archivo.txt"
+
