dc78d2627221cea4482688f9aa15fc684f4719cc
Dehivid12 <dehividsalgado2022@gmail.com>
2025-12-03 16:00:57 +0000

    Explicación de script2.sh

--- script2.sh
+++ script2.sh
@@ -1,4 +1,7 @@
-uname -a
-lscpu
-free -h
-df -h
+uname -a      # Muestra información del sistema operativo y del kernel
+
+lscpu         # Muestra información del procesador 
+
+free -h       # Muestra el uso de la memoria RAM y swap en formato legible 
+
+df -h         # Muestra el uso de las particiones del disco en formato legible
